30d39aa22baa5000bd4921474e301c1bae1f360b
hammafataka <66084806+hammafataka@users.noreply.github.com>
2021-05-08 23:49:58 +0200

    puting guesters and long press features to label and image, getting countries name in the service

--- Countries_In_World/Countries_In_World/ViewModels/DetailViewModel.cs
+++ Countries_In_World/Countries_In_World/ViewModels/DetailViewModel.cs
@@ -9,12 +9,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace Countries_In_World.ViewModels
 {
     public class DetailViewModel : BaseViewModel
     {
+
+        private int score;
+
+        public int Score
+        {
+            get { return score; }
+            set { SetProperty(ref score, value); }
+        }
+
 
         private List<Geoname> countryoptions;
 
@@ -33,6 +43,30 @@ namespace Countries_In_World.ViewModels
         }
         public ICommand LoadDataCommand { private set; get; }
         public ICommand selectionChangedCommand { private set; get; }
+        public ICommand TouchedCommand { private set; get; }
+        public ICommand LabelTouchedCommand { private set; get; }
+
+
+        public async Task labeltouched()
+        {
+            await Share.RequestAsync(new ShareTextRequest
+            {
+                Text = SelectedGeoname.countryName+"\n"+selectedGeoname.currencyCode+"\n"+SelectedGeoname.continent,
+                Title = SelectedGeoname.countryName+" Details"
+            });
+        }
+        public async Task touched()
+        {
+            if (SelectedGeoname != null)
+            {
+                string answer = await App.Current.MainPage.DisplayPromptAsync("Guess","Guess the country capital..");
+                if (SelectedGeoname.capital.ToLower() == answer)
+                {
+                    score += 5;
+                }
+            }
+            
+        }
         public async Task LoadData()
         {
 
@@ -43,10 +77,12 @@ namespace Countries_In_World.ViewModels
         }
         public DetailViewModel()
         {
+            Score = new int();
             CountryOptions = new List<Geoname>();
             SelectedGeoname = new Geoname();
             Task.Run(async () => await LoadData());
-            
+            TouchedCommand = new Command(async () => await touched());
+            LabelTouchedCommand = new Command(async () => await labeltouched());
         }
 
     }
